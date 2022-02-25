0d9dbb0f13e1270d8f18d7cfb67d03a5aa9a6761
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2022-02-25 11:07:29 -0500

    Update erp_dash.py

--- erp_dash.py
+++ erp_dash.py
@@ -56,9 +56,9 @@ if file is not None:
             chart_2.add_trace(go.Pie(labels=df_3['Area'], values=df_3['Profit']),1, 3)
 
             chart_2.update_traces(hole=.6, hoverinfo="label+percent")
-            chart_2.update_layout(annotations=[dict(text='Round 1', x=0.18, y=0.5, font_size=20, showarrow=False),
-                                               dict(text='Round 2', x=0.82, y=0.5, font_size=20, showarrow=False),
-                                               dict(text='Round 3', x=1.46, y=0.5, font_size=20, showarrow=False)])
+            chart_2.update_layout(annotations=[dict(text='Round 1', x=0.0.1, y=0.5, font_size=20, showarrow=False),
+                                               dict(text='Round 2', x=0.3, y=0.5, font_size=20, showarrow=False),
+                                               dict(text='Round 3', x=0.6, y=0.5, font_size=20, showarrow=False)])
 
 
         return chart_2
