187107e7e51d8dcb83b391760d233eb08d1b66b7
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2022-02-25 08:53:31 -0500

    Update erp_dash.py

--- erp_dash.py
+++ erp_dash.py
@@ -32,7 +32,7 @@ if file is not None:
         data_f['Profit'] = data_f['Value'] - data_f['Cost']
         st.subheader('Profit by Region')
         chart_2 = px.line(data_f, x = 'Day', y = 'Profit', color = 'Material description', facet_row = 'Round', facet_col = 'Area', 
-                          template = 'seaborn', width = 1200)
+                          template = 'seaborn', width = 1800)
 
         return chart_2
     
