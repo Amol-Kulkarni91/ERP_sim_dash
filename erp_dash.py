2a9a7078ef9a16a02249ced511a44ffb8ab5d862
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2022-02-25 11:14:02 -0500

    Update erp_dash.py

--- erp_dash.py
+++ erp_dash.py
@@ -59,7 +59,7 @@ if file is not None:
             chart_2.update_layout(annotations=[dict(text='Round 1', x=0.08, y=0.5, font_size=20, showarrow=False),
                                                dict(text='Round 2', x=0.5, y=0.5, font_size=20, showarrow=False),
                                                dict(text='Round 3', x=0.93, y=0.5, font_size=20, showarrow=False)],
-                                 width = 1000, height = 800)
+                                 width = 800, height = 800)
 
            
         return chart_2
