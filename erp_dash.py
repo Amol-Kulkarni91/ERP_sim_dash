7b48cbf02fb8f1dd8ef7be1170bd195a24867610
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2022-02-26 14:45:36 -0500

    Update erp_dash.py

--- erp_dash.py
+++ erp_dash.py
@@ -156,36 +156,36 @@ if file is not None:
 			day_1_pure = st.sidebar.number_input('Scheduled Delivery of 1L ClearPure', min_value = 1, max_value = 60, step = 1)
 			submit_button = st.form_submit_button(label='Submit')
 
-	if len(df['Round'].unique()) == 1:
-		new_data = wide_data(df)
-		new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
-		new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
-		new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
-		new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
-		new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
-		new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
-		st.plotly_chart(inv_chart(first_round(new_data)))
+		if len(df['Round'].unique()) == 1:
+			new_data = wide_data(df)
+			new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
+			new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
+			new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
+			new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
+			new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
+			new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
+			st.plotly_chart(inv_chart(first_round(new_data)))
 
-	elif len(df['Round'].unique()) == 2:
-		new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
-		new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
-		new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
-		new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
-		new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
-		new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
-		st.plotly_chart(inv_chart(second_round(new_data)))
-	else:
+		elif len(df['Round'].unique()) == 2:
+			new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
+			new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
+			new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
+			new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
+			new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
+			new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
+			st.plotly_chart(inv_chart(second_round(new_data)))
+		else:
 
-		new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
-		new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
-		new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
-		new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
-		new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
-		new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
-		st.plotly_chart(inv_chart(third_round(new_data)))
+			new_data.loc[day_5_spritz,'500mL Spritz'] = new_data.loc[day_5_spritz,'500mL Spritz'] + (ml_5_spritz*24)
+			new_data.loc[day_5_lemspritz,'500mL Lemon Spritz'] = new_data.loc[day_5_lemspritz,'500mL Spritz'] + (ml_5_lemspritz*24)
+			new_data.loc[day_5_pure,'500mL ClearPure'] = new_data.loc[day_5_pure,'500mL Lemon Spritz'] + (ml_5_pure*24)
+			new_data.loc[day_1_spritz,'1L Spritz'] = new_data.loc[day_1_spritz,'500mL ClearPure'] + (l_1_spritz*12)
+			new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] = new_data.loc[day_1_lemspritz,'1L Lemon Spritz'] + (l_1_lemspritz*12)
+			new_data.loc[day_1_pure,'1L ClearPure'] = new_data.loc[day_1_pure,'1L ClearPure'] + (l_1_pure*12)
+			st.plotly_chart(inv_chart(third_round(new_data)))
 
-	st.plotly_chart(profit_product(df))
-	st.plotly_chart(dem_product(df))
+		st.plotly_chart(profit_product(df))
+		st.plotly_chart(dem_product(df))
 
 	else:
 
