fbbd8dfedf108cc85bba24599f98e2959a9787ee
Amol Kulkarni <16808069+Amol-Kulkarni91@users.noreply.github.com>
2022-03-01 10:49:10 -0500

    Update erp_dash.py

--- erp_dash.py
+++ erp_dash.py
@@ -76,61 +76,61 @@ if file is not None:
 		
 		for i in data_f1.iterrows():
 			if i[1]["Material Description"] == "500mL Spritz":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df1.loc[(temp_df1['Round'] == 1) & (temp_df1['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df1.loc[(temp_df1['Round'] == 2) & (temp_df1['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df1.loc[(temp_df1['Round'] == 3) & (temp_df1['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df1.loc[(temp_df1['Round'] == temp_df1['Round'].unique().max()) & (temp_df1['Day'] == 1), 'Qty'] -= i[1]['Quantity']
 					
 			elif i[1]["Material Description"] == "500mL Lemon Spritz":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df2.loc[(temp_df2['Round'] == 1) & (temp_df2['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df2.loc[(temp_df2['Round'] == 2) & (temp_df2['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df2.loc[(temp_df2['Round'] == 3) & (temp_df2['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df2.loc[(temp_df2['Round'] == temp_df2['Round'].unique().max()) & (temp_df2['Day'] == 1), 'Qty'] -= i[1]['Quantity']
 					
 			elif i[1]["Material Description"] == "500mL ClearPure":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df3.loc[(temp_df3['Round'] == 1) & (temp_df3['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df3.loc[(temp_df3['Round'] == 2) & (temp_df3['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df3.loc[(temp_df3['Round'] == 3) & (temp_df3['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df3.loc[(temp_df3['Round'] == temp_df3['Round'].unique().max()) & (temp_df3['Day'] == 1), 'Qty'] -= i[1]['Quantity']
 					
 			elif i[1]["Material Description"] == "1L Spritz":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df4.loc[(temp_df4['Round'] == 1) & (temp_df4['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df4.loc[(temp_df4['Round'] == 2) & (temp_df4['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df4.loc[(temp_df4['Round'] == 3) & (temp_df4['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df4.loc[(temp_df4['Round'] == temp_df4['Round'].unique().max()) & (temp_df4['Day'] == 1), 'Qty'] -= i[1]['Quantity']
 				
 			elif i[1]["Material Description"] == "1L Lemon Spritz":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df5.loc[(temp_df5['Round'] == 1) & (temp_df5['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df5.loc[(temp_df5['Round'] == 2) & (temp_df5['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df5.loc[(temp_df5['Round'] == 3) & (temp_df5['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df5.loc[(temp_df5['Round'] == temp_df5['Round'].unique().max()) & (temp_df5['Day'] == 1), 'Qty'] -= i[1]['Quantity']
 					
 			elif i[1]["Material Description"] == "1L ClearPure":
-				if i[1]['Goods'][0] == 1:
+				if i[1]['Goods'][0] == '1':
 					temp_df6.loc[(temp_df6['Round'] == 1) & (temp_df6['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
-				elif i[1]['Goods'][0] == 2:
+				elif i[1]['Goods'][0] == '2':
 					temp_df6.loc[(temp_df6['Round'] == 2) & (temp_df6['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']		
-				elif i[1]['Goods'][0] == 3:
+				elif i[1]['Goods'][0] == '3':
 					temp_df6.loc[(temp_df6['Round'] == 3) & (temp_df6['Day'] == int(i[1]['Goods'][2:4])), 'Qty'] -= i[1]['Quantity']
 				elif i[1]['Goods'] == 'N.Sch':
 					temp_df6.loc[(temp_df6['Round'] == temp_df6['Round'].unique().max()) & (temp_df6['Day'] == 1), 'Qty'] -= i[1]['Quantity']
